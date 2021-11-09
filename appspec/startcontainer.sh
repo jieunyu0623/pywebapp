2cde2bf97a8d6ae54d903be185151beb88b38c95
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2021-11-09 12:50:23 -0800

    Update startcontainer.sh
    d

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 708906220194.dkr.ecr.us-west-2.amazonaws.com
-docker pull 170015466808.dkr.ecr.us-west-2.amazonaws.com/jieunyu0623/pywebapp:latest || {
+docker pull 708906220194.dkr.ecr.us-west-2.amazonaws.com/jieunyu0623/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
