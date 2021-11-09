73a71386e6018f1d9c84583528a7dd399340ab12
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2021-11-09 13:41:00 -0800

    Update pywebapp.py
    d

--- pywebapp.py
+++ pywebapp.py
@@ -14,7 +14,7 @@ class Default:
         return """<html>
         <title>PyWebApp</title>
         <body bgcolor="white">
-        <h1>Jieun Yu A00998343</h1>
+        <h1>A00998343 Jieun Yu</h1>
         <p>Available API methods:</p>
         <ul>
         <li><a href="/api/v1/test">/api/v1/test</a></li>
